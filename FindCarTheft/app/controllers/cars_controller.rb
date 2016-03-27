72035d8fbfe1f07d1f8210c326abb04a7c16c029
romavov <rom.vovna@gmail.com>
2016-03-27 19:29:45 +0300

    viev category

--- FindCarTheft/app/controllers/cars_controller.rb
+++ FindCarTheft/app/controllers/cars_controller.rb
@@ -2,7 +2,12 @@ class CarsController < ApplicationController
   before_action :find_car, only: [:show, :edit, :update, :destroy]
 
   def index
-    @cars = Car.all.order("created_at DESC")
+    if params[:category].blank?
+      @cars = Car.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @cars = Car.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
