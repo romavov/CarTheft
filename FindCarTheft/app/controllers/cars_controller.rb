92e70426e742bdb66df132a50d3da72822dd0c6e
romavov <rom.vovna@gmail.com>
2016-03-29 19:31:25 +0300

    linking user to car

--- FindCarTheft/app/controllers/cars_controller.rb
+++ FindCarTheft/app/controllers/cars_controller.rb
@@ -21,6 +21,7 @@ class CarsController < ApplicationController
 
   def create
     @car = Car.new(params.require(:car).permit!)
+    @car.update_attribute(:user_id, current_user.id)
     if @car.save
       redirect_to @car
     else
